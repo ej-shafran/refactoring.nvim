83ce65ddeba61f011e9e8a4e16502841c3ac5914
ej-shafran <ejshafran@hilma.tech>
2023-09-12 15:20:08 +0300

    Add Lua test

---
+++ lua/refactoring/tests/refactor/221/lua/example/rename.expected.lua
@@ -0,0 +1,8 @@
+local function add(a)
+    local some_var = 10
+    -- TODO: this line doesn't work yet:
+    -- x = x + 1
+    return a + some_var
+end
+
+add(10)
---
+++ lua/refactoring/tests/refactor/221/lua/example/rename.start.lua
@@ -0,0 +1,8 @@
+local function add(a)
+    local x = 10
+    -- TODO: this line doesn't work yet:
+    -- x = x + 1
+    return a + x
+end
+
+add(10)
